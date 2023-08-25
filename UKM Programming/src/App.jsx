6c18c2cc04398f84119f6ad19b0de41c573eac1d
camiliayosefaa <cyosefaa@gmail.com>
2023-08-25 16:00:17 +0700

    Tugas 2 (Fetch Data) belum selesai

--- UKM Programming/src/App.jsx
+++ UKM Programming/src/App.jsx
@@ -2,36 +2,130 @@ import './App.css'
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
+
 function App() {
 
-  const [quotes, setQuotes] = useState(null);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-
     const fetchData = async () => {
-      const res = await axios.get("https://strapi-rygs.onrender.com/api/quotes")
-      setQuotes(res.data.data)
+      //biar keliatan kalo error, pake try catch
+      try {
+        const data = await axios.get("https://strapi-rygs.onrender.com/api/prodis");
+        const resData = data.data.data
+        const prodiData = resData[0].attributes.prodi[0][0]
+        setData(prodiData)
+      } catch (error) {
+        console.error("Error!!", error)
+      }
     }
-
     fetchData()
-
   }, []);
+  console.log(data);
 
   return (
+    <>
     <div>
-      {quotes?.map((item, index) => (
-        
-        <div>
-          {item.attributes.Quote}
-          <br />
-          - {item.attributes.Author}
-          <br />
-        </div>
-
-      ))}
-
-
+      <table border={0}>
+        <tr>{data.nama_prodi}</tr>
+        <tr>Kepala Prodi: {data.kepala_prodi}</tr>
+        <tr>Sekretaris: {data.sektretaris}</tr>
+      </table>
     </div>
+    <br />
+
+    {data.mahasiswa && data.mahasiswa.map((item, index)=>(
+      <div key={index}>
+        <span>Angkatan: {item.tahun_masuk}</span><br />
+        <span>Kelas Pagi</span>
+        <table border={1}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NPM</th>
+              <th>Nama</th>
+              <th>Jenis Kelamin</th>
+              <th>Alamat</th>
+              <th>Hobi</th>
+            </tr>
+          </thead>
+          <tbody>
+            {item.data.pagi.map((item, index)=>(
+              <tr key={index}>
+                <td>{index+1}</td>
+                <td>2125250001</td>
+                <td>{item.nama}</td>
+                <td>{item.jenis_kelamin}</td>
+                <td>{item.alamat}</td>
+                <td>{item.hobi.join(", ")}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <br />
+
+        <span>Kelas Malam</span>
+        <table border={1}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NPM</th>
+              <th>Nama</th>
+              <th>Jenis Kelamin</th>
+              <th>Alamat</th>
+              <th>Hobi</th>
+            </tr>
+          </thead>
+          <tbody>
+            {item.data.malam.map((item, index)=>(
+              <tr key={index}>
+                <td>{index+1}</td>
+                <td>2125250001</td>
+                <td>{item.nama}</td>
+                <td>{item.jenis_kelamin}</td>
+                <td>{item.alamat}</td>
+                <td>{item.hobi.join(", ")}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <br />
+
+        <span>Kelas Cuti</span> <br />
+        {item.data.cuti.length > 0?(
+          <table border={1}>
+            <thead>
+            <tr>
+              <th>ID</th>
+              <th>NPM</th>
+              <th>Nama</th>
+              <th>Jenis Kelamin</th>
+              <th>Alamat</th>
+              <th>Hobi</th>
+            </tr>
+          </thead>
+          <tbody>
+            {item.data.cuti.map((item, index)=>(
+              <tr key={index}>
+                <td>{index+1}</td>
+                <td>2125250001</td>
+                <td>{item.nama}</td>
+                <td>{item.jenis_kelamin}</td>
+                <td>{item.alamat}</td>
+                <td>{item.hobi.join(", ")}</td>
+              </tr>
+            ))}
+          </tbody>
+          </table>
+        ):(
+          <span>Tidak ada Mahasiswa yang mengambil kelas ini.</span>
+        )}
+        <br />
+      </div>
+    ))}
+    </>
   );
 }
+  
+
 export default App
